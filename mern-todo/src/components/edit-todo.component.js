fad9908b627ff9491000d963bf0fd58140b243a8
ahewardmills76 <ahewardmills76@gmail.com>
2020-08-21 21:23:53 -0500

    created components and associated routes for ListTodo, CreateTodo, and EditTodos

---
+++ mern-todo/src/components/edit-todo.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class EditTodos extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the Edit-Todas Component</p>
+            </div>
+        )
+    }
+
+}
